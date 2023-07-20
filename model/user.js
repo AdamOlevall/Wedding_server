a3a01857cb479ea261846963488c1f4a0482b8a4
Adam Olevall <adamolevall@adams-mac.home>
2023-07-20 12:11:48 +0200

    remove fields from schema

--- model/user.js
+++ model/user.js
@@ -1,17 +1,9 @@
 const mongoose = require('mongoose');
 
 const User = mongoose.Schema({
-    dayBefore: {
-        type: String,
-        required: [true, 'dayBefore is required']
-    },
     firstName: {
         type: String,
         required: [true, 'firstName is required']
-    },
-    fish: {
-        type: String,
-        required: [true, 'fish is required']
     },
     lastName: {
         type: String,
@@ -20,26 +12,6 @@ const User = mongoose.Schema({
     allergy: {
         type: String,
         required: [false]
-    },
-    mail: {
-        type: String,
-        required: [true, 'mail is required']
-    },
-    meat: {
-        type: String,
-        required: [true, 'meat is required']
-    },
-    phone: {
-        type: String,
-        required: [true, 'phone is required']
-    },
-    vegan: {
-        type: String,
-        required: [true, 'vegan is required']
-    },
-    vegetarian:{
-        type: String,
-        required: [true, 'vegetarian is required']
     },
     weddingDay: {
         type: String,
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
-const nodemailer = require('nodemailer');
 const User = require('./model/user');
 const config = require('./config.json');
 const bodyParser = require('body-parser');
@@ -11,34 +10,12 @@ app.use(cors());
 
 const jsonParser = bodyParser.json();
 
-var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'adamolevall@gmail.com',
-        pass: 'ynwa1892'
-    }
-});
-
-
 app.get('/attenders', async (req, res) => {
     const users = await User.find();
     res.send(users);
 });
 
 app.post('/post-form', jsonParser, async (req, res) => {
-    const mailOptions = {
-        from: 'adamolevall@gmail.com',
-        to: 'einarssonwedding@gmail.com',
-        subject: req.body.firstName + ' ' + req.body.lastName,
-        text: `Namn: ${req.body.firstName} ${req.body.lastName}\nMail: ${req.body.mail}\nTelefon: ${req.body.phone}\nKommer dagen innan: ${req.body.dayBefore}\nKommer på bröllopet: ${req.body.weddingDay}\nTransport: ${req.body.transport}\nKött: ${req.body.meat}\nFisk: ${req.body.fish}\nVegan: ${req.body.vegan}\nVegetariskt: ${req.body.vegetarian}\nAllergier: ${req.body.allergy}`,
-    };
-    await transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    });
   try {
     const myUser = new User(req.body);
     await myUser.save();
